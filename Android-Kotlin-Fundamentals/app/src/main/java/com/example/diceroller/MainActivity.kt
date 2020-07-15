aa50b8e8d628e03d44f1574a86178b45eac81c13
Yacine Souilah <ysoui@hotmail.fr>
2020-07-15 13:23:44 +0100

    Optimization

--- Android-Kotlin-Fundamentals/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ Android-Kotlin-Fundamentals/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -8,9 +8,13 @@ import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        diceImage = findViewById(R.id.dice_image)
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener{ rollDice() }
     }
@@ -19,7 +23,6 @@ class MainActivity : AppCompatActivity() {
         // Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
 
         val randomInt = (1..6).random()
-        val diceImage: ImageView = findViewById(R.id.dice_image)
         val drawableResource = when (randomInt) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
